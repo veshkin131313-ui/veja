3088d1db3d9a3a33b62ce2c26e84fc61a97c80fa
veshkin131313-ui <veshkin131313@mail.ru>
2025-09-10 20:53:40 +0300

    пункт 2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def convert(amount, from_currency, to_currency):
     return data["rates"][to_currency]
 
 
+#Вводим сумму
 print("Конвертер валют")
 while True:
     try:
@@ -15,9 +16,46 @@ while True:
         break
     except ValueError:
         print("Нужно ввести число!")
-from_cur = input("Из какой валюты (например, USD): ").upper()
-to_cur = input("В какую валюту (например, EUR): ").upper()
+
+
+#список доступных валют
+def get_currencies():
+    url= "https://api.frankfurter.app/currencies"
+    try:
+        response = requests.get(url)
+        data = response.json()
+        return list(data.keys())
+    except Exception as e:
+        raise RuntimeError(f"Ошибка епта: {e}")
+currencies = get_currencies()
+print("Доступные валюты:", " | ".join(currencies))
+
+
+#Из какой валюты берем
+while True:
+    from_cur = input(f"Из какой валюты:").upper()
+    if len(from_cur) !=3:
+        print("Код валюты должен состоять из 3 букв")
+        continue
+    if from_cur in currencies:
+        break
+    else:
+        print("Такой валюты нет, нужно выбрать другую")
+
+
+#В какую валюту
+while True:
+    to_cur = input(f"В какую валюту:").upper()
+    if len(to_cur) !=3:
+        print("Код валюты должен состоять из 3 букв")
+        continue
+    if to_cur in currencies:
+        break
+    else:
+        print("Такой валюты нет, нужно выбрать другую")
+
 
 result = convert(amount, from_cur, to_cur)
 decimals = int(input("Сколько знаков после запятой выводить? "))
-print(f"{amount} {from_cur} = {result:.{decimals}f} {to_cur}")
+formatted_result = f"{result:.{decimals}f}".rstrip("0").rstrip(".")
+print(f"{amount} {from_cur} = {formatted_result} {to_cur}")
